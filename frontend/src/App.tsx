31b41a1e5c611ba811114325a53a52ed11b7f01e
altcomvis <allanlteixeira@gmail.com>
2024-12-01 09:35:36 -0300

    Add to Vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import EditPedido from './components/edit-pedido'
 import AddPedido from './components/add-pedido'
+import EditPedido from './components/edit-pedido'
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
